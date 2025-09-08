a751df7591f8c17bcad3a414c6fc1473d066667f
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-09-07 22:49:06 -0400

    Journal #2 Full Functionality Achieved @ 29:26.08
    Each new set of cubes is fed into an ArrayList, when a new set of cubes is generated, every cube in the ArrayList is destroyed.
    I also remembered to save the scene before pushing things to the git this time.
    
    "Welcome to my house! As you can see I've knocked over many chairs because I get so tilted at the towers..."
    - Memphis, Tennessee

--- Prog3_JournalProject_Ethan_Powell/Assets/Scripts/BoxSpawnerScript.cs
+++ Prog3_JournalProject_Ethan_Powell/Assets/Scripts/BoxSpawnerScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using TMPro;
-
+using System.Collections;
 
 public class BoxSpawnerScript : MonoBehaviour
 {
@@ -8,6 +8,8 @@ public class BoxSpawnerScript : MonoBehaviour
     private int boxesToRender = 0;
     [SerializeField] TMP_InputField boxInputField;
     [SerializeField] GameObject boxPrefab;
+
+    ArrayList boxCatalogue = new ArrayList();
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,6 +33,13 @@ public class BoxSpawnerScript : MonoBehaviour
 
         if(newCount > 0 && newCount <= 9 ) {
 
+            foreach (GameObject oldBox in boxCatalogue)
+            {
+
+                GameObject.Destroy(oldBox);
+
+            }
+
             boxesToRender = newCount;
 
             for (int i = 1; i <= boxesToRender; i++)
@@ -41,6 +50,8 @@ public class BoxSpawnerScript : MonoBehaviour
                 currentLocation.x = currentLocation.x + (1.5f * i);
 
                 newBox.transform.position = currentLocation;
+
+                boxCatalogue.Add(newBox);
 
             }
 
