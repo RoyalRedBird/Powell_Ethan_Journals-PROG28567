19997a634d7198d241c2b15ea539422b3dd01aee
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-09-21 17:25:33 -0400

    Journals 3-3 and 3-4 completed. Apologies for not spacing out pushes, I was getting into a flow.

--- Prog3_JournalProject_Ethan_Powell/Assets/Scripts/Controllers/Asteroid.cs
+++ Prog3_JournalProject_Ethan_Powell/Assets/Scripts/Controllers/Asteroid.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
@@ -8,13 +9,48 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Vector2 currentWaypoint;
+    [SerializeField] Vector2 currentDirection;
+
+    private void Start()
     {
+        
+        SetWaypoint();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        AsteroidMovement();
+
     }
+
+    public void AsteroidMovement()
+    {
+
+        Vector3 velocity;
+
+        velocity = currentDirection * moveSpeed * Time.deltaTime * -1;
+
+        if (Vector3.Distance(transform.position, currentWaypoint) < arrivalDistance) { 
+        
+            SetWaypoint();
+        
+        }
+
+        transform.position += velocity;
+
+    }
+
+    public void SetWaypoint()
+    {
+
+        currentDirection = Vector3.Normalize(new Vector2(Random.Range(1f, -1f), Random.Range(1f, -1f)));
+
+        currentWaypoint = new Vector2((transform.position.x - (currentDirection.x * maxFloatDistance)), (transform.position.y - (currentDirection.y * maxFloatDistance)));
+
+    }
+
 }
--- Prog3_JournalProject_Ethan_Powell/Assets/Scripts/Controllers/Stars.cs
+++ Prog3_JournalProject_Ethan_Powell/Assets/Scripts/Controllers/Stars.cs
@@ -6,9 +6,31 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    float currentTime = 0;
+    int transformIndexer = 0;
 
     // Update is called once per frame
     void Update()
     {
+
+        currentTime += Time.deltaTime;
+        Vector2 targetPoint = Vector2.Lerp(starTransforms[transformIndexer].position, starTransforms[transformIndexer + 1].position, currentTime/drawingTime);
+        Debug.DrawLine(starTransforms[transformIndexer].position, targetPoint, Color.white);
+
+        if (currentTime >= drawingTime) { 
+        
+            transformIndexer++;
+            currentTime = 0;
+
+            if(transformIndexer == starTransforms.Count - 1)
+            {
+
+                transformIndexer = 0;
+
+            }
+        
+        }
+
     }
+
 }
