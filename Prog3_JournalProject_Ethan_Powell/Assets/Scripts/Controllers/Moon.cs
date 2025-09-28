c757d4ebc6008f4d8fc949c567eb3fa66ff388e9
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-09-28 17:05:44 -0400

    Journal 4-3 Completed in 11:37.00

--- Prog3_JournalProject_Ethan_Powell/Assets/Scripts/Controllers/Moon.cs
+++ Prog3_JournalProject_Ethan_Powell/Assets/Scripts/Controllers/Moon.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    public float orbitSpeed;
+    public float orbitRadius;
+    [SerializeField] float currentAngle;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +19,22 @@ public class Moon : MonoBehaviour
     void Update()
     {
 
+        OrbitHandler(orbitRadius, orbitSpeed);
+
     }
+
+    void OrbitHandler(float radius, float speed)
+    {
+
+        float angleIncrement = speed * Time.deltaTime;
+        float incrementAsRadians = angleIncrement * Mathf.Deg2Rad;
+        currentAngle += angleIncrement;
+
+        Vector3 currentPos = planetTransform.position;
+        currentPos += new Vector3(Mathf.Cos(currentAngle), Mathf.Sin(currentAngle)) * radius;
+
+        transform.position = currentPos;
+
+    }
+
 }
